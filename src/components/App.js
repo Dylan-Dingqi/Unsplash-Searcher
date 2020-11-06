1c3a7456dc3f11a8e6161dd178c9088138097927
Dylan <Dylan-Dingqi@users.noreply.github.com>
2020-11-06 14:24:01 -0500

    Corrected speling

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: "10px"}}>
                 <h1 className="ui center aligned header" style={{marginTop: "1.2em", marginBottom:"0.3em"}}>Unsplash Image Searcher</h1>
-                <h4  className="ui center aligned header" style={{marginTop:"0", marginBottom:"3em"}}>A unage search tool that uses Unsplash API</h4>
+                <h4  className="ui center aligned header" style={{marginTop:"0", marginBottom:"3em"}}>A image search tool that uses Unsplash API to find the image you want</h4>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
                 {
                     this.state.isLoading?
